34c7bebfc3dff55aede80fb98ae37b1c71ea215d
bony_musau <boniface.musau@moringaschool.com>
2023-10-03 23:39:31 +0300

    Arithmetic Lab assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+num1=31;
+num2=2;
+var multipy = num1 * num2;
+console.log(multipy);
+
+var random = Math.floor(Math.random() * 20) + 1;
+console.log(random);
+
+var num3 = 22;
+var num4 = 9; 
+var mod = (num3 % num4)
+console.log(mod);
+ 
+
+var max = Math.max(2, 6, 15, 20, 11);
+console.log(max)
